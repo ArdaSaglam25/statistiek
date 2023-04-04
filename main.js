87271da2e82093d7a46069503bf7a3dd8919d1fa
ArdaSaglam25 <302427819@student.rocmondriaan.nl>
2023-04-04 11:57:44 +0200

    klaar opdracht 1

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("main.js loaded 🎉");
+console.log("main.js loaded");
+
+
+const myArray = [ 15, 14, 31, 14, 5, 26, 7, 48, 29, 11, 13, 14, 35];
+
+
+console.log(myArray);
+
+let som = 0;
+
+for (let i = 0; i < myArray.length; i++) {
+    const getal = myArray[i];
+    som = som + getal;
+}
+
+const average = som/myArray.length;
+
+console.log("Gemiddelde is: ", average);
